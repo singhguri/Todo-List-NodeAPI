9c20e810c0e15a20c23eeebeefd2f492ddf72117
singhguri5598@gmail.com <singhguri5598@gmail.com>
2020-07-26 02:00:52 +0530

    Initial Project

---
+++ api/models/todoListModel.js
@@ -0,0 +1,25 @@
+"use strict";
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    required: "Kindly enter the name of the task.",
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now(),
+  },
+  status: {
+    type: [
+      {
+        type: String,
+        enum: ["Pending", "Ongoing", "Completed"],
+      },
+    ],
+    default: ["Pending"],
+  },
+});
+
+module.exports = mongoose.model("Tasks", TaskSchema);
